1f74e4d793e6373d45fe949fc08d0c35c1729a06
Michal <michal.klebek@gmail.com>
2025-03-31 17:39:38 +0200

    Add languages to LSP

--- init.lua
+++ init.lua
@@ -682,9 +682,16 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
         --
-
+        ts_ls = {}, -- tsserver is deprecated
+        html = { filetypes = { 'html', 'twig', 'hbs' } },
+        cssls = {},
+        tailwindcss = {},
+        dockerls = {},
+        sqlls = {},
+        terraformls = {},
+        jsonls = {},
+        yamlls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
