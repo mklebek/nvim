e282ec4d449b671a148e077bfd9bd16a4018d78d
Michal <michal.klebek@gmail.com>
2025-04-10 14:49:56 +0200

    Add autotag

--- init.lua
+++ init.lua
@@ -933,7 +933,13 @@ require('lazy').setup({
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
+  -- Auto close and change HTML tags
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
