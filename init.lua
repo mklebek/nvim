70945bff5d9073519b75373ef20938545a40b91c
Michal <michal.klebek@gmail.com>
2025-04-11 10:49:18 +0200

    Add file operations

--- init.lua
+++ init.lua
@@ -744,6 +744,20 @@ require('lazy').setup({
     end,
   },
 
+  { -- File operations (rename, move etc...) improvements for auto renaming imports
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Uncomment whichever supported plugin(s) you use
+      -- "nvim-tree/nvim-tree.lua",
+      'nvim-neo-tree/neo-tree.nvim',
+      -- "simonmclean/triptych.nvim"
+    },
+    config = function()
+      require('lsp-file-operations').setup()
+    end,
+  },
+
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
